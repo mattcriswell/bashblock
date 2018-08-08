a294641e965e29cd8a377d81bbdef22812935633
matt.criswell <llewsircttam@gmail.com>
2018-08-07 22:29:56 -0500

    switching over to a while loop

--- mine.sh
+++ mine.sh
@@ -1,22 +1,12 @@
 #!/bin/bash 
-echo $'hello \n\a' 
+echo $'hello\n' 
 
-declare -A butts
-
-butts[salt]=123
-butts[old_hash]="abcd123basdf"
-butts[something]="wut m8?"
-
-echo -n ${butts[salt]} ${butts[something]} ${butts[old_hash]}
-
-echo ""
-echo ""
 upper_range=100000
 block=2
 nounce=salty
 data="the payload"
-hash_flag=000
-echo ""
-echo -n $(openssl rand -hex 8)$block$nounce$data
-echo ""
-for i in {1..10000}; do echo -n $(openssl rand -hex 8)${block}${nounce}${data}| sha256sum; done | egrep ^${hash_flag}
+hash_flag=00
+echo "mining for hashes starting with $hash_flag"
+while true; do
+	echo -n $(openssl rand -hex 8)${block}${nounce}${data}|sha256sum |egrep ^${hash_flag}
+done
