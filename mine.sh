5c857f4b2f503113640dc018a5c78a9c4cc08ebc
matt.criswell <llewsircttam@gmail.com>
2018-08-07 22:40:12 -0500

    adding some additional logic to primary loop

--- mine.sh
+++ mine.sh
@@ -1,6 +1,4 @@
 #!/bin/bash 
-echo $'hello\n' 
-
 upper_range=100000
 block=2
 nounce=salty
@@ -8,5 +6,8 @@ data="the payload"
 hash_flag=00
 echo "mining for hashes starting with $hash_flag"
 while true; do
-	echo -n $(openssl rand -hex 8)${block}${nounce}${data}|sha256sum |egrep ^${hash_flag}
+	potential_hash=$(echo -n $(openssl rand -hex 8)${block}${nounce}${data}|sha256sum |egrep ^${hash_flag})
+	if [[ $? == 0 ]]
+		then echo FOUND: ${potential_hash};
+	fi
 done
