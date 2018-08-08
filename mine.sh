161e4b9faecb3fd596cddf17840472c50a68f5eb
matt.criswell <llewsircttam@gmail.com>
2018-08-07 23:40:26 -0500

    more polishing of the main loop

--- mine.sh
+++ mine.sh
@@ -1,13 +1,25 @@
 #!/bin/bash 
-upper_range=100000
+num_pow=3
+solutions=()
 block=2
 nounce=salty
 data="the payload"
 hash_flag=00
-echo "mining for hashes starting with $hash_flag"
+found_count=0
+echo "mining for ${num_pow} hashes starting with $hash_flag"
 while true; do
-	potential_hash=$(echo -n $(openssl rand -hex 8)${block}${nounce}${data}|sha256sum |egrep ^${hash_flag})
+	#Put all our items together, hash them, and see if it meets the criteria
+	found_hash=$(echo -n $(openssl rand -hex 8)${block}${nounce}${data}|sha256sum |egrep ^${hash_flag})
+	#If our grep was a success, add the hash to the solutions array (after we clean it up some)
 	if [[ $? == 0 ]]
-		then echo FOUND: ${potential_hash};
+	then solutions+=($(echo ${found_hash}|tr -d '   -')) && let "found_count++"; 
+			if [[ ${found_count} -ge ${num_pow} ]]
+				then break; 
+			fi
 	fi
+done
+echo "First found solutions:"
+for i in "${solutions[@]}"
+do
+	echo $i
 done
